60e0df362f883e6f52178eec8bc75fd2c16a6959
Frank Li <lzaasmt@gmail.com>
2021-10-22 18:19:30 -0600

    Add app to Firestore
    Syntax issue happened, need change accordingly

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/MainActivity.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 }
