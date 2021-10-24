b7de7d77034ba9c401b2f1052ead9f302668fa72
“Weijia <weijia4@ualberta.ca>
2021-10-24 16:29:55 -0600

    change for build successfully

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/DashBoardActivity.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/DashBoardActivity.java
@@ -1,4 +1,14 @@
 package com.example.happyhabitapp;
 
-public class DashBoardActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
 }
