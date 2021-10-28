5b37ab4b82cf1862a0a4a5b93a02e222d45ac978
Anthony Nguyen <anthony3@ualberta.ca>
2021-10-28 15:56:49 -0600

    Added CustomHabitList class

---
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/CustomHabitList.java
@@ -0,0 +1,42 @@
+package com.example.happyhabitapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomHabitList extends ArrayAdapter<Habit> {
+
+    private ArrayList<Habit> habits;
+    private Context context;
+
+    /**
+     * This is a constructor for a Custom list of Habits
+     * @param context
+     * @param habit
+     *      ArrayList of Habits
+     */
+    public CustomHabitList(Context context, ArrayList<Habit> habit) {
+        super(context,0, habit);
+        this.habits = habit;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View view = convertView;
+        
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.)
+        }
+
+        return view;
+    }
+}
