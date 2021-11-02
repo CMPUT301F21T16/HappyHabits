d8bc1809d06c6af0fa9bf5aa046733dd3552c481
Frank Li <lzaasmt@gmail.com>
2021-11-02 13:25:12 -0600

    Add HabitEvent class, Add DashBoard activity

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/DashBoard.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/DashBoard.java
@@ -3,6 +3,7 @@ package com.example.happyhabitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        ImageView user_prof = (ImageView) findViewById(R.id.user_profile_pic);
+        user_prof.setBackgroundResource(R.drawable.lol);
     }
+
+
 }
