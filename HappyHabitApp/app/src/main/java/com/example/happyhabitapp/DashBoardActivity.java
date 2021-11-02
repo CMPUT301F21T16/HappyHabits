3ad1b3e775f848713ae1ed19f80153471f9f54f1
“Weijia <weijia4@ualberta.ca>
2021-11-02 11:58:10 -0600

    progress on dashboard

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/DashBoardActivity.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/DashBoardActivity.java
@@ -3,6 +3,8 @@ package com.example.happyhabitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DashBoardActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class DashBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        Button  TransferToList_Button;
+
+        TransferToList_Button = findViewById(R.id.TranferToList_Button);
     }
 }
