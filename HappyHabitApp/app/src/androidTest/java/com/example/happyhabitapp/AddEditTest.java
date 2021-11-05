c1bdd3987769b780843ecd8e21ea1b360abe2f9e
anujpradeep <pradeepanuj123@gmail.com>
2021-11-05 03:42:29 -0600

    Finished AddTest
    Co-Authored-By: Armaan Mangat <75745159+Armaan-Mangat@users.noreply.github.com>

--- HappyHabitApp/app/src/androidTest/java/com/example/happyhabitapp/AddEditTest.java
+++ HappyHabitApp/app/src/androidTest/java/com/example/happyhabitapp/AddEditTest.java
@@ -4,31 +4,36 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
-import static org.hamcrest.core.AllOf.allOf;
-import static org.hamcrest.core.AnyOf.anyOf;
 import static org.junit.Assert.assertTrue;
-
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import org.junit.After;
+import com.robotium.solo.Solo ;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class AddEditTest {
+
+    private Solo solo;
+
     @Rule
-    public ActivityScenarioRule<HabitActivity> activityScenarioRule
-            = new ActivityScenarioRule<>(HabitActivity.class);
+    public final ActivityTestRule<HabitActivity> activityRule = new ActivityTestRule<>(HabitActivity.class);
+
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
 
     @Test
     public void testAdd(){
@@ -52,6 +57,7 @@ public class AddEditTest {
                 e.printStackTrace();
             }
         }
+
         /**
          * find Add Habit button and click it to add habit to list
          */
@@ -64,35 +70,10 @@ public class AddEditTest {
             }
         }
 
-    }
-    @Test
-    public void testEdit(){
-        onView(withId(R.id.add_habit_btn)).perform(click());
-
-        onView(withId(R.id.habit_title_editText))
-                .perform(typeText("Title"), closeSoftKeyboard());
-
-        onView(withId(R.id.habit_reason_editText))
-                .perform(typeText("Reason"), closeSoftKeyboard());
-
-        UiDevice device = UiDevice.getInstance(getInstrumentation());
-        UiObject chooseDays = device.findObject(new UiSelector().text("W"));
-        if (chooseDays.exists()){
-            try{
-                chooseDays.click();
-            } catch (UiObjectNotFoundException e){
-                e.printStackTrace();
-            }
-        }
-
-        UiObject addHabit = device.findObject(new UiSelector().text("ADD HABIT"));
-        if (addHabit.exists()){
-            try{
-                addHabit.click();
-            } catch (UiObjectNotFoundException e){
-                e.printStackTrace();
-            }
-        }
+        // Reads on the screen for Title, Reason, and Wednesday
+        assertTrue(solo.waitForText("Title",1,10)
+                && solo.waitForText("Reason",1,10)
+                && solo.waitForText("Wednesday",1,10));
 
     }
 
