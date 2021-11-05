c6d72472efb732fe651df11cab2817a0bb1d813d
Armaan Mangat <armaanm1010@gmail.com>
2021-11-05 03:42:31 -0600

    finished EditTest
    Co-authored-by: anujpradeep <apradeep@ualberta.ca>

--- HappyHabitApp/app/src/androidTest/java/com/example/happyhabitapp/AddEditTest.java
+++ HappyHabitApp/app/src/androidTest/java/com/example/happyhabitapp/AddEditTest.java
@@ -1,70 +1,41 @@
 package com.example.happyhabitapp;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
-import static org.hamcrest.core.AllOf.allOf;
-import static org.hamcrest.core.AnyOf.anyOf;
 import static org.junit.Assert.assertTrue;
-
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import org.junit.After;
+import com.robotium.solo.Solo ;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class AddEditTest {
+    private Solo solo;
+
     @Rule
-    public ActivityScenarioRule<HabitActivity> activityScenarioRule
-            = new ActivityScenarioRule<>(HabitActivity.class);
+    public ActivityTestRule<HabitActivity> rule
+            = new ActivityTestRule<>(HabitActivity.class);
 
-    @Test
-    public void testAdd(){
-        onView(withId(R.id.add_habit_btn)).perform(click()); //click add button
-
-        onView(withId(R.id.habit_title_editText)) // put "Title" in habit title Edit text box
-                .perform(typeText("Title"), closeSoftKeyboard());
-
-        onView(withId(R.id.habit_reason_editText)) // put "Reason" in habit reason Edit text box
-                .perform(typeText("Reason"), closeSoftKeyboard());
-
-        /**
-         * find object with "W" and click it to select Wednesday from day selector
-         */
-        UiDevice device = UiDevice.getInstance(getInstrumentation());
-        UiObject chooseDays = device.findObject(new UiSelector().text("W"));
-        if (chooseDays.exists()){
-            try{
-                chooseDays.click();
-            } catch (UiObjectNotFoundException e){
-                e.printStackTrace();
-            }
-        }
-        /**
-         * find Add Habit button and click it to add habit to list
-         */
-        UiObject addHabit = device.findObject(new UiSelector().text("ADD HABIT"));
-        if (addHabit.exists()){
-            try{
-                addHabit.click();
-            } catch (UiObjectNotFoundException e){
-                e.printStackTrace();
-            }
-        }
-
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
+
     @Test
     public void testEdit(){
         onView(withId(R.id.add_habit_btn)).perform(click());
@@ -84,7 +55,9 @@ public class AddEditTest {
                 e.printStackTrace();
             }
         }
-
+        /**
+         * find add habit button and click it
+         */
         UiObject addHabit = device.findObject(new UiSelector().text("ADD HABIT"));
         if (addHabit.exists()){
             try{
@@ -93,6 +66,54 @@ public class AddEditTest {
                 e.printStackTrace();
             }
         }
+        /**
+         * click habit
+         */
+        onView(ViewMatchers.withId(R.id.habit_list))
+            .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+
+        onView(withId(R.id.habit_title_editText))
+                .perform(clearText(),typeText("newTitle"), closeSoftKeyboard());
+
+        onView(withId(R.id.habit_reason_editText))
+                .perform(clearText(),typeText("newReason"), closeSoftKeyboard());
+        /**
+         * deselect wednesday
+         */
+        chooseDays = device.findObject(new UiSelector().text("W"));
+        if (chooseDays.exists()){
+            try{
+                chooseDays.click();
+            } catch (UiObjectNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+        /**
+         * find F button from day selector and click it
+         */
+        chooseDays = device.findObject(new UiSelector().text("F"));
+        if (chooseDays.exists()){
+            try{
+                chooseDays.click();
+            } catch (UiObjectNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+        /**
+         * find done editing button and click it
+         */
+        UiObject editHabit = device.findObject(new UiSelector().text("DONE EDITING"));
+        if (editHabit.exists()){
+            try{
+                editHabit.click();
+            } catch (UiObjectNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+
+        assertTrue(solo.waitForText("newTitle",1,10)
+                && solo.waitForText("newReason",1,10)
+                && solo.waitForText("Friday",1,10));
 
     }
 
