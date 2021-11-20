fcf1dde2940a0374f55951e56c6524d9ece2d2e4
jd-lo <jdlo@ualberta.ca>
2021-11-19 20:01:38 -0700

    Dashboard/Habit hybrid activity XML

---
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/MergedDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.happyhabitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MergedDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merged_display);
+    }
+}
