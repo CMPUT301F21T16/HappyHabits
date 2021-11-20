31000b715a6380293856e549a9cab501451ae08d
jd-lo <jdlo@ualberta.ca>
2021-11-20 02:27:47 -0700

    Made habit viewmodel prettier.

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/MergedDisplayActivity.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/MergedDisplayActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class MergedDisplayActivity extends AppCompatActivity {
 
+    private User currentUser;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merged_display);
     }
+
+
+
+
+
 }
