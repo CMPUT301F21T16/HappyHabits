dccbfc996f5432581f35bee05c1dc484407075e2
jd-lo <jdlo@ualberta.ca>
2021-11-21 22:18:04 -0700

    Added habit event classes and XML. Completed list element XML.

---
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/HabitEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.happyhabitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HabitEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_event);
+    }
+}
