2017c1ef11dbf06e7c2e26d36eaf0c923f165862
jd-lo <jdlo@ualberta.ca>
2021-11-24 06:00:36 -0700

    Habit event activity is largely functional. No firebase. Weird adapter persists.

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/EventsAdapter.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/EventsAdapter.java
@@ -61,5 +61,9 @@ public class EventsAdapter extends RecyclerView.Adapter implements ItemTouchHelp
     public void setTouchHelper(ItemTouchHelper helper) {
         this.touchHelper = helper;
     }
+
+    public List<HabitEvent> getEventList() {
+        return habitEventList;
+    }
 }
 
--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/HabitEventActivity.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/HabitEventActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class represents the activity that displays all of the habit's events
@@ -105,9 +106,20 @@ public class HabitEventActivity extends AppCompatActivity implements HabitListen
         });
     }
 
+    /**
+     * This method launches the fragment to edit the clicked event
+     * @param position
+     */
     @Override
     public void onHabitClick(int position) {
         //TODO: Go to fragment
+
+        DialogFragment newFragment = new HabitEventFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("Edit Event", passedInHabit);
+        newFragment.setArguments(args);
+
+        newFragment.show(getSupportFragmentManager(),"EDIT_EVENT");
     }
 
     /**
@@ -122,6 +134,9 @@ public class HabitEventActivity extends AppCompatActivity implements HabitListen
 
     @Override
     public void editEvent(HabitEvent newEvent, HabitEvent oldEvent) {
-
+        List<HabitEvent> dataList = recyclerAdapter.getEventList();
+        int pos = dataList.indexOf(oldEvent);
+        dataList.set(pos, newEvent);
+        recyclerAdapter.notifyDataSetChanged();
     }
 }
