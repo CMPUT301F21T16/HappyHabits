5570ed82d6ea4479e228f1e1cc85dc6dd704d476
jd-lo <jdlo@ualberta.ca>
2021-11-27 02:24:55 -0700

    Added progress bar to the habits according to respective events

--- HappyHabitApp/app/src/main/java/com/example/happyhabitapp/HabitViewHolder.java
+++ HappyHabitApp/app/src/main/java/com/example/happyhabitapp/HabitViewHolder.java
@@ -1,11 +1,13 @@
 package com.example.happyhabitapp;
 
 import android.content.ClipData;
+import android.content.res.ColorStateList;
 import android.media.Image;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
@@ -21,16 +23,19 @@ import androidx.recyclerview.widget.RecyclerView;
 public class HabitViewHolder extends RecyclerView.ViewHolder implements
     View.OnTouchListener, GestureDetector.OnGestureListener
 {
-
     GestureDetector habitGestureDetector;
+
+    private View view;
 
     private TextView titleTextView;
     private TextView reasonTextView;
     private TextView frequencyTextView;
-    private ImageButton dragHandle;
 
     private ItemTouchHelper touchHelper;
     private HabitListener viewListener;
+
+    private ProgressBar progressBar;
+    private TextView progressBarText;
 
     /**
      * Gets all relevant data fields and initializes a {@link GestureDetector} to set to the View.
@@ -40,7 +45,7 @@ public class HabitViewHolder extends RecyclerView.ViewHolder implements
      */
     public HabitViewHolder(View habitView, ItemTouchHelper helper, HabitListener habitListener) {
         super(habitView);
-
+        view = habitView;
         titleTextView = (TextView) habitView.findViewById(R.id.habit_title);
         reasonTextView = (TextView) habitView.findViewById(R.id.reason_text);
         frequencyTextView = (TextView) habitView.findViewById(R.id.selected_dates);
@@ -60,7 +65,67 @@ public class HabitViewHolder extends RecyclerView.ViewHolder implements
         titleTextView.setText(habit.getTitle());
         reasonTextView.setText(habit.getReason());
         frequencyTextView.setText(habit.getWeekAsStr());
+        setProgressOnBar(habit);
     }
+
+    private void setProgressOnBar(Habit habit) {
+        int percentage = getProgressOnBar(habit);
+        progressBar = view.findViewById(R.id.progress_bar);
+        progressBarText = view.findViewById(R.id.progress_text);
+        progressBar.setProgress(percentage);
+        fillProgressBar(percentage);
+    }
+
+    /**
+     * Colors the progress bar using its percentage
+     * @param percentage an int representing the percentage of the progress bar
+     */
+    private void fillProgressBar(int percentage) {
+        if (percentage < 33) {
+            progressBarText.setTextColor(view.getContext().getResources().getColor(R.color.progress_indicator_low));
+            progressBar.setProgressTintList(ColorStateList.valueOf(view.getContext().getResources().getColor(R.color.progress_indicator_low)));
+        }
+        else if (percentage < 66) {
+            progressBarText.setTextColor(view.getContext().getResources().getColor(R.color.progress_indicator_mid));
+            progressBar.setProgressTintList(ColorStateList.valueOf(view.getContext().getResources().getColor(R.color.progress_indicator_mid)));
+        }
+        else {
+            progressBarText.setTextColor(view.getContext().getResources().getColor(R.color.progress_indicator_high));
+            progressBar.setProgressTintList(ColorStateList.valueOf(view.getContext().getResources().getColor(R.color.progress_indicator_high)));
+        }
+    }
+
+    /**
+     * Determines what portion of the bar is filled, and what color it is to be set to.
+     */
+    private int getProgressOnBar(Habit habit) {
+        int progressCap = habit.getEvents().size();
+        float totalProgress = 0;
+
+        if (progressCap == 0) {
+            progressBar.setVisibility(View.INVISIBLE);  //If there are no respective events, progress bar will not show up.
+        }
+        else {
+            progressBar.setMax(progressCap);
+        }
+
+        //Gets the total progress of the event
+        for (int i = 0; i < progressCap - 1; i++) {
+            int status = habit.getEvents().get(i).getStatus();  //Get the status code of the habit event
+
+            switch (status) {
+                case 0: totalProgress += 0;     //Event is incomplete
+                        break;
+                case 1: totalProgress += 1;     //Event is completed
+                        break;
+                case 2: totalProgress += 0.5;   //Event is in progress
+                        break;
+            }
+        }
+
+        return Math.round(totalProgress/progressCap) * 100;  //Gets a rounded percentage out of 100
+    }
+
 
     //All the below methods are required by the interfaces implemented
 
